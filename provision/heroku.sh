802236c01954f3bda99028bfa8a323cb6432923d
rkiel <cygnus@pobox.com>
2014-04-27 11:43:52 -0400

    updated heroku provision

--- provision/heroku.sh
+++ provision/heroku.sh
@@ -3,7 +3,14 @@ echo "Installing Heroku toolbelt"
 wget -qO- https://toolbelt.heroku.com/install.sh | /bin/bash
 echo "Heroku toolbelt installed"
 
+echo "Adding Heroku toolbelt to bashrc"
+echo 'PATH="/usr/local/heroku/bin:$PATH"' >> /home/vagrant/.profile
+echo "Heroku toolbelt added to bashrc"
+
 echo "Installing Heroku gems"
-gem install netrc --no-rdoc --no-ri
-gem install heroku-api --no-rdoc --no-ri
+cd /vagrant
+echo "gem 'netrc',       '~> 0.7.7'"  >> Gemfile
+echo "gem 'rest_client', '~> 1.7.3'"  >> Gemfile
+echo "gem 'heroku-api',  '~> 0.3.18'" >> Gemfile
 echo "Heroku gems installed"
+
