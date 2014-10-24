e015284a4dcf682e85e54b493cdfb1bb262285ae
Robert Kiel <cygnus@pobox.com>
2014-10-23 20:26:49 -0400

    Update node.sh
    Added install of gcc/make/build-essential to node.sh

--- provision/node.sh
+++ provision/node.sh
@@ -14,6 +14,9 @@ apt-get install -y nodejs >/dev/null 2>&1
 echo "  installing npm"
 apt-get install -y npm >/dev/null 2>&1
 
+echo "  installing gcc make build-essential"
+apt-get install -y gcc make build-essential
+
 echo "  installing grunt"
 npm install -g grunt-cli >/dev/null 2>&1
 
