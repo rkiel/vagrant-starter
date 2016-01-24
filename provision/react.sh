d9d695d77487c263bf927f7e88a771940aba39a2
rkiel <cygnus@pobox.com>
2016-01-23 21:09:52 -0500

    updates for react

--- provision/react.sh
+++ provision/react.sh
@@ -26,11 +26,21 @@ npm install lodash --save >/dev/null 2>&1
 echo "  installing webpack"
 npm install webpack --save-dev >/dev/null 2>&1
 
+# the core logic of Babel
 echo "  installing babel-core"
 npm install babel-core --save-dev >/dev/null 2>&1
 
+# You can use Babel with Webpack easily
 echo "  installing babel-loader"
 npm install babel-loader --save-dev >/dev/null 2>&1
+
+# Out of the box Babel doesn't do anything.
+# In order to actually do anything to your code you need to enable plugins.
+# There are two types of plugins: syntax and transform.
+# Syntax allow Babel to parse additional syntax whereas transform apply transformations.
+# This way the code that is using future syntax can get transformed back to JavaScript older environments can understand.
+# To make it easier to consume plugins, Babel supports the concept of presets.
+# Each preset comes with a set of plugins so you don't have to wire them up separately.
 
 echo "  installing babel-preset-es2015"
 npm install babel-preset-es2015 --save-dev >/dev/null 2>&1
@@ -39,6 +49,6 @@ echo "  installing babel-preset-react"
 npm install babel-preset-react --save-dev >/dev/null 2>&1
 
 echo "  installing babel-preset-stage-1"
-npm install babel-preset-react --save >/dev/null 2>&1
+npm install babel-preset-stage-1 --save >/dev/null 2>&1
 
 echo "react installed"
